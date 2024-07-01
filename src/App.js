3193b170f473886568e3bde4d6ead699a35260c0
Sesughun <sesughunlan@gmail.com>
2024-07-01 14:23:40 -0700

    Colourful layout for quote

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+
 function App() {
   function handleNewQuote() {}
   return (
-    <div className="App">
-      <div id="quote-box">
-        <div id="text"></div>
-        <div id="author"></div>
-        <button id="new-quote" onClick={handleNewQuote}></button>
-        <a id="tweet-quote"></a>
+    <div className="card" id="entire">
+      <div id="quote-box" className="card-body">
+        <div id="text">To go up is to go up, up up!</div>
+        <div id="author">-Lan Sesughun</div>
+        <button id="new-quote" onClick={handleNewQuote}>
+          New quote
+        </button>
+        <a id="tweet-quote">Tweet</a>
       </div>
     </div>
   );
